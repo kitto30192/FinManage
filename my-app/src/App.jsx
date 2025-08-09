b1e5ab26696260b1977890874ebf283920cf1c2b
kitto30192 <Kittopra40@gmail.com>
2025-08-09 16:09:06 +0530

    chanage to vite+react

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       <p>hello</p>
+      </div>
+    </>
+  )
+}
+
+export default App
